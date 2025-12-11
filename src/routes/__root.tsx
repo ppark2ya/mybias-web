69e36560e9e26721b9b93906e9913a913e3c6ad2
ppark2ya <vigabhan1231@gmail.com>
2025-12-11 21:49:45 +0900

    tanstack router devtools 배포 버전에는 안나오게 수정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,16 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { lazy } from "react";
+
+// 1. 배포 환경(production)이 아닐 때만 Devtools를 로드하도록 설정
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // 배포 시에는 아무것도 렌더링하지 않음 (null)
+  : lazy(() =>
+      // 개발 모드에서만 패키지를 로딩함 (번들 사이즈 감소 효과)
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // 예전 버전의 경우: default: res.TanStackRouterDevtoolsPanel
+      }))
+    );
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,4 +19,4 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
