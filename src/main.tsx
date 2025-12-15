b2813fe089523b923ab6bab6401e35217c271422
ppark2ya <vigabhan1231@gmail.com>
2025-12-15 22:59:51 +0900

    다국어 관련 설정 추가 - 다국어를 처리하기 위한 라이브러리 추가 (i18next react-i18next i18next-browser-languagedetector) - 메인 타이틀 변경: 나만의 최애 - 언어에 따라 동적으로 메타정보 변경을 위한 라이브러리 추가 (react-helmet-async)

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
+import "./i18n";
+import { HelmetProvider } from "react-helmet-async";
 
-import { queryClient } from './lib/query-client'
-import { routeTree } from './routeTree.gen'
+import { queryClient } from "./lib/query-client";
+import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </HelmetProvider>
   </StrictMode>
-)
+);
